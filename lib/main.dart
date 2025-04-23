8bf9cc47ac82f917868d7294da9beda6dac80f04
Aroba Dar <arubad309@gmail.com>
2025-04-24 01:48:56 +0500

    organizer dynamically fetched and get direction working

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,3 @@
-// // import 'package:event_ease/auth/register.dart';
-// // import 'package:event_ease/auth/register_get.dart';
-// //import 'package:event_ease/test.dart';
-// import 'package:flutter/material.dart';
-// import 'home_page.dart'; // Import the HomePage file
-
-// void main() {
-//   runApp(EventEaseApp());
-// }
-
-// class EventEaseApp extends StatelessWidget {
-//   const EventEaseApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Event Ease',
-//       theme: ThemeData(
-//         primaryColor: Color(0xFF6D62F4),
-//         fontFamily: 'Arial', // Replace with your preferred font
-//       ),
-//       home: HomePage(), // Set HomePage as the initial screen
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'auth/login.dart'; // Your new login/register page
 import 'home_page.dart'; // Your existing home page
